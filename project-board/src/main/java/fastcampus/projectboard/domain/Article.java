650ba6afdce8c97f9ccec0f027275f64a7ce1dce
혜민\forbe <hyeomin9326@gmail.com>
2023-01-15 23:17:14 +0900

    #10

--- project-board/src/main/java/fastcampus/projectboard/domain/Article.java
+++ project-board/src/main/java/fastcampus/projectboard/domain/Article.java
@@ -1,9 +1,20 @@
 package fastcampus.projectboard.domain;
 
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@ToString
+@Table(indexes = )
 public class Article {
 
+    @Id
     private Long id;
     private String title;
     private String content;
