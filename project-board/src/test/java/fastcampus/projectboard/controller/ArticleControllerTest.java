00bcb1e3ba85d91c453c893ce79e5da86c30be67
혜민\forbe <hyeomin9326@gmail.com>
2023-01-16 11:58:39 +0900

    #16 게시글 관련 뷰 테스트 작성

---
+++ project-board/src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,34 @@
+package fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("뷰 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class) //해당 컨트롤러만 읽어옴
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    void test() throws Exception {
+        //given
+
+        //when & then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+}
