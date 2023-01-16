11e5f5ba16fa1f6a24b793941a4b495797133506
혜민\forbe <hyeomin9326@gmail.com>
2023-01-16 11:48:35 +0900

    #1 Querydsl 지원 이용하여  api 검색 옵션 추가
    QuerydslPredicateExecutor
    QuerydslBinderCustomizer

--- project-board/src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
+++ project-board/src/main/java/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,33 @@
 package fastcampus.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import fastcampus.projectboard.domain.Article;
+import fastcampus.projectboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long> ,
+        QuerydslPredicateExecutor<Article>, //엔티티 안에 있는 모든 필드에 대한 기본 검색 기능 추가
+        QuerydslBinderCustomizer<QArticle> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true);
+
+        //포함된 필드만 검색기능 추가함
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
