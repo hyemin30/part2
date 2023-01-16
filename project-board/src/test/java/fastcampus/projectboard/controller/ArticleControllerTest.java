a6f4cf6a7d333359ee72e5c1c8088dca7031b9e3
혜민\forbe <hyeomin9326@gmail.com>
2023-01-16 13:14:19 +0900

    #16 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러 테스트에 반영

--- project-board/src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ project-board/src/test/java/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package fastcampus.projectboard.controller;
 
+import fastcampus.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("뷰 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class) //해당 컨트롤러만 읽어옴
 class ArticleControllerTest {
 
@@ -34,4 +37,6 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
+
+
 }
